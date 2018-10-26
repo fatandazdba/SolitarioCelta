16ae871f0be6cc8d0bcbdbacd464258949a0974e
Freddy Tandazo <freddy.tandazo.yanez@alumnos.upm.es>
2018-10-26 14:26:45 +0200

    first commit

---
+++ app/src/main/java/es/upm/miw/SolitarioCelta/MainActivity.java
@@ -0,0 +1,131 @@
+package es.upm.miw.SolitarioCelta;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+	JuegoCelta mJuego;
+    private final String CLAVE_TABLERO = "TABLERO_SOLITARIO_CELTA";
+
+	private final int[][] ids = {
+		{       0,        0, R.id.p02, R.id.p03, R.id.p04,        0,        0},
+        {       0,        0, R.id.p12, R.id.p13, R.id.p14,        0,        0},
+        {R.id.p20, R.id.p21, R.id.p22, R.id.p23, R.id.p24, R.id.p25, R.id.p26},
+        {R.id.p30, R.id.p31, R.id.p32, R.id.p33, R.id.p34, R.id.p35, R.id.p36},
+        {R.id.p40, R.id.p41, R.id.p42, R.id.p43, R.id.p44, R.id.p45, R.id.p46},
+        {       0,        0, R.id.p52, R.id.p53, R.id.p54,        0,        0},
+        {       0,        0, R.id.p62, R.id.p63, R.id.p64,        0,        0}
+	};
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mJuego = new JuegoCelta();
+        mostrarTablero();
+
+    }
+
+    /**
+     * Se ejecuta al pulsar una posición
+     *
+     * @param v Vista de la posición pulsada
+     */
+    public void posicionPulsada(View v) {
+        String resourceName = getResources().getResourceEntryName(v.getId());
+        int i = resourceName.charAt(1) - '0';
+        int j = resourceName.charAt(2) - '0';
+
+        mJuego.jugar(i, j);
+
+        mostrarTablero();
+        if (mJuego.juegoTerminado()) {
+            new AlertDialogFragment().show(getFragmentManager(), "ALERT DIALOG");
+        }
+    }
+
+    /**
+     * Visualiza el tablero
+     */
+    public void mostrarTablero() {
+        RadioButton button;
+
+        for (int i = 0; i < JuegoCelta.TAMANIO; i++)
+            for (int j = 0; j < JuegoCelta.TAMANIO; j++)
+                if (ids[i][j] != 0) {
+                    button = findViewById(ids[i][j]);
+                    button.setChecked(mJuego.obtenerFicha(i, j) == 1);
+                }
+    }
+
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString(CLAVE_TABLERO, mJuego.serializaTablero());
+        super.onSaveInstanceState(outState);
+    }
+
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String grid = savedInstanceState.getString(CLAVE_TABLERO);
+        mJuego.deserializaTablero(grid);
+        mostrarTablero();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.opciones_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuAbout:
+                startActivity(new Intent(this, About.class));
+                return true;
+            case R.id.preferences:
+                startActivity(new Intent(this, SCeltaPreferences.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void aceptar() {
+        Toast t=Toast.makeText(this,"EL juego ha sido reiniciado.", Toast.LENGTH_SHORT);
+        t.show();
+        mJuego.reiniciar();
+        this.mostrarTablero();
+    }
+
+    public void cancelar() {
+        //finish();
+        Toast t=Toast.makeText(this,"Favor, continue con el juego.", Toast.LENGTH_SHORT);
+        t.show();
+    }
+
+    public void reiniciar_juego(View view) {
+        AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
+        dialogo1.setTitle("Importante");
+        dialogo1.setMessage("¿ El programa será reiniciado, desea continuar ?");
+        dialogo1.setCancelable(false);
+        dialogo1.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogo1, int id) {
+                aceptar();
+            }
+        });
+        dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogo1, int id) {
+                cancelar();
+            }
+        });
+        dialogo1.show();
+
+    }
+}
